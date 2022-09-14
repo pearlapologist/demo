4c8e7300e3f491dcb963b964a559a25ac3d41d17
wirigiwewe <addict.hamster@gmail,com>
2022-09-14 22:40:37 +0600

    самая первая задача

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,12 +1,16 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
     @GetMapping("/home")
-    public String home(){
+    public String home(@RequestParam("first_number") int first_number,@RequestParam("second_number") int second_number, Model model) {
+        model.addAttribute("add", first_number + second_number);
+        model.addAttribute("mul", first_number*second_number);
         return "home";
     }
 }
