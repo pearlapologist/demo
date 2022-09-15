12c3176b9ad946ea9a9aa3b6f94da13866ed3912
wirigiwewe <addict.hamster@gmail,com>
2022-09-15 23:06:34 +0600

    Циклы zadachi

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -14,10 +14,39 @@ import java.util.Map;
 public class HomeController {
 
     @GetMapping("/home")
-    public String home(Model model) { //''ru                                  'en'
-        String[][] twoDimArray = {{"пон","вт","ср","чт","птн","сб","вск"}, {"mon","tuesday","wednesday","thursday","friday","saturday","sunday"}};
-        model.addAttribute("val1", twoDimArray[0][0]);
-        model.addAttribute("val2", twoDimArray[1][2]);
+    public String home(Model model) {
+        System.out.println("#1");
+        String[] array = new String[10];
+       for(int i=0;i<10;i++){
+           array[i]="x";
+           System.out.println(array[i]);
+       }
+        System.out.println("#2");
+        int[] array2 = new int[10];
+        for(int i=0;i<10;i++){
+            array2[i]=i+1;
+            System.out.println(array2[i]);
+        }
+        System.out.println("#3");
+        String[] array3 = {"html", "css", "php", "js", "jq"};
+        for (String w : array3) {
+            System.out.print(w +"\n");
+        }
+
+        System.out.println("#4");
+        int[] array4 = {1, 2, 3, 4, 5};
+        int result=0;
+        for (int t : array4) {
+            result+=t*t;
+        }
+        System.out.println(result);
+
+        System.out.println("#5");
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("Kolya", 200);
+        map.put("Vasya", 300);
+        map.put("Petya", 400);
+        map.forEach((k, v) -> System.out.println((k + " - salary " + v+"$")));
         return "home";
     }
 
