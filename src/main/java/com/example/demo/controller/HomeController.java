df86ad6e61394d1111745c1b7a5e1d010e730eeb
wirigiwewe <addict.hamster@gmail,com>
2022-09-15 08:42:25 +0600

    1.Дан массив с элементами 'Привет, ', 'мир' и '!'. Необходимо вывести на экран фразу 'Привет, мир!'

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -3,24 +3,18 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/home")
-    public String home(@RequestParam("number") int number, @RequestParam("country") int country,
-                       @RequestParam("dis") String dis, Model model) {
-        double res = number * 400;
-        if (country == 3) {
-            res += res * 0.01;
-        } else if (country == 2) {
-            res += res * 0.12;
-        }
-        if (dis.equals("yes")) {
-            res -= (res * 0.05);
-        }
-        model.addAttribute("res", res);
+    public String home(Model model) {
+        String[] array = new String[] { "Привет,", "мир", "!"};
+        model.addAttribute("array", array);
         return "home";
     }
+
 }
